a5282b36d8adbbf9953f5486f2a464a398f1acfc
lepythoner <56467460+lepythoner@users.noreply.github.com>
2020-07-26 17:07:53 +0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,14 +3,8 @@ import subprocess
 import platform
 
 target = input("Enter an IP or Host to ping:")
-
-if platform == 'Windows' :
-
-    host = subprocess.Popen(['ping', target], stdout = subprocess.PIPE).communicate()[0]
-
-else :
-
-    host = subprocess.Popen(['ping', target, "-c4",], stdout = subprocess.PIPE).communicate()[0]
+print("Wait....")
+host = subprocess.Popen(['ping', target], stdout = subprocess.PIPE).communicate()[0]
 
 print(host)
 
