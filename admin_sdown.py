2d4ef3c64de017c109a083a72b21ad3c9511cefa
lepythoner <56467460+lepythoner@users.noreply.github.com>
2020-08-01 17:15:15 +0400

    Add files via upload

---
+++ admin_sdown.py
@@ -0,0 +1,50 @@
+#For administration and maintainence purposes ONLY
+
+import winsound
+from colorama import *
+import time
+import os
+
+init()
+
+print(Fore.GREEN + "Loading...")
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+
+print(Fore.GREEN + "Loading Complete")
+time.sleep(5)
+
+print(Fore.GREEN + "Ip Address of Person")
+input1000 = input("")
+
+print(Fore.GREEN + "Message")
+input2000 = input("")
+
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+winsound.Beep(500, 500)
+winsound.Beep(1000, 250)
+time.sleep(5)
+os.system("shutdown /m " + input1000 + " /r /c  " + input2000 + "")
+print("Task done")
+
+
+
