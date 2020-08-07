bfb3484e448ac27155a468dbaaca3e43a6f6cd09
lepythoner <56467460+lepythoner@users.noreply.github.com>
2020-08-07 20:34:14 +0400

    Add files via upload

---
+++ atomic_time.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+print("By Sahal Mulki")
+options = Options()
+options.headless = True
+cap = DesiredCapabilities().FIREFOX
+
+driver = webdriver.Firefox(options=options, capabilities=cap, executable_path=r"C:\Users\M.Sahal\Desktop\geckodriver.exe")
+driver.get("https://time.is/just")
+
+time = driver.find_element_by_id('time_section')
+print(time.text)
+
+driver.quit()
+
+
+
+
+
+
+
