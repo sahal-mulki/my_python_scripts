97a2dad86a36f058321205ff69646f4f7c2e8119
lepythoner <56467460+lepythoner@users.noreply.github.com>
2020-08-10 15:13:58 +0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,7 +2,9 @@ import subprocess
 
 import platform
 
-print("By Sahal Mulki")
+__author__ = "Sahal Mulki"
+
+print("Made by Sahal Mulki")
 target = input("Enter an IP or Host to ping:")
 print("Wait....")
 host = subprocess.Popen(['ping', target], stdout = subprocess.PIPE).communicate()[0]
