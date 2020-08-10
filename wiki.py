db4314ee467895b439f2efba5bd1e6dd99aa9bda
lepythoner <56467460+lepythoner@users.noreply.github.com>
2020-08-10 15:14:54 +0400

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,5 +1,7 @@
 import wikipedia
 
-print("By Sahal Mulki")
+__author__ = "Sahal Mulki"
+
+print("Made by Sahal Mulki")
 input1 = input ("What do you need a Wikipedia summary on?")
 print(wikipedia.summary(input1))
