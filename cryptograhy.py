6c09745d32f967d8955cd87fbfd3777bb0fc5f00
sahal-mulki <sahal.mulki@gmail.com>
2020-09-24 09:59:44 +0400

    Completed encryyption(forgot to by mistake)

--- cryptograhy.py
+++ cryptograhy.py
@@ -1,17 +1,15 @@
-# USE AT YOUR OWN RISK
-# THIS IS NOT FULLY SECURE ENCRYPTION
-# IF ITS A REALLY IMPORTANT THING USE 
-# A MORE SECURE ENCRYPTION ALGORITHM
-
 from cryptography.fernet import Fernet
+import time
+import pyperclip
 
+def make_unicode(input2):
+    input2 = input2.decode('utf-8')
+    return input2
 
 key = Fernet.generate_key()
 f = Fernet(key)
 
-__author__ = "Sahal Mulki"
 
-print("Made by Sahal Mulki")
 input1 = input("(E)ncrypt or (D)ecrypt")
 
 
@@ -25,8 +23,40 @@ if input1 == "E":
     if input2 == "":
         key = key.decode('utf-8')
         print("Here's your key : " + key)
+        print("Its pasted to your clipboard paste it somewhere")
+        pyperclip.copy(key)
+        inputnoi = input("Press Enter when you are done pasting")
     secure_secret = f.encrypt(secret)
     secure_secret = str(secure_secret, 'utf-8')
-    print("Encryption complete , here's the encrypted string : " + str(secure_secret))
 
-    
+    print("Encrypting......")
+    print("[O     ]")
+    time.sleep(1)
+    print("[OO    ]")
+    time.sleep(0.25)
+    print("[OOO   ]")
+    time.sleep(0.25)
+    print("[OOOO  ]")
+    time.sleep(0.25)
+    print("[OOOOO ]")
+    time.sleep(0.25)
+    print("[OOOOOO]")
+    print("Encryption complete , here's the encrypted string : " + str(secure_secret))
+    print("String copied to clipboard")
+    pyperclip.copy(str(secure_secret))
+
+if input1 == "D":
+    secret1 = input("Copy your encrypted string to your clipboard and press Enter")
+    secret = pyperclip.paste()
+    key1 = input("Copy your key to your clipboard and press ENTER")
+    key = pyperclip.paste()
+    secret2 = bytes(secret, 'utf-8')
+    f = Fernet(key)
+    print("The decrypted string is :")
+    final = f.decrypt(secret2)
+    final2 = make_unicode(final)
+    print(final2)
+    print("String copied to clipboard")
+    pyperclip.copy(final2)
+
+input1000000 = input("")
